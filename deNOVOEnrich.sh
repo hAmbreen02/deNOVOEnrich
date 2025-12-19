5cbf9359d2e214a5a6d2d107c63b7075c8f74152
hAmbreen02 <heena.ambreen02@gmail.com>
2025-12-19 14:39:37 +0000

    Update deNOVOEnrich.sh

--- deNOVOEnrich.sh
+++ deNOVOEnrich.sh
@@ -37,14 +37,16 @@ echo " --Sample			Name of the sample "
 echo " --adapter			Path to Illumina adapter sequences file (e.g., TruSeq3-PE.fa)"
 echo " --outDir			Directory to store all output results"
 echo " --TEfam			Transposon family of Interest" 
-echo "				(Must match TE-specific Samplenames example: Use AtCopia93 for"
-echo "				AtCopia93_loci.bed or AtCopia93_flanking_sequence.fa"
+echo "				    (Must match TE-specific Samplenames example: Use AtCopia93 for"
+echo "				    AtCopia93_loci.bed or AtCopia93_flanking_sequence.fa"
+echo " --somatic                        Coverage threshold to call somatic insertions (Default = 5)"
+echo " --heritable                      Coverage threshold to call non-reference segregating insertions (Default = 30)"
 echo " --monomeric_repeat		Bed file with coordinates of monomeric repeats of reference genome"
 echo " --CORES			Number of threads to use"
 echo " --help |-h			Display this help message"
 echo ""
 echo "Example:"
-echo "  bash deNOVOEnrich.sh --Sample col1 --TEfam AtCopia93 --genome /path/genome.fa --ref_TE /path/TE_files --rawRead1 A1_1.fq --rawRead2 A1_2.fq --adapter /path/TruSeq3-PE.fa --outDir ./results --CORES 8"
+echo "  bash deNOVOEnrich.sh --Sample col1 --TEfam AtCopia93 --genome /path/genome.fa --ref_TE /path/TE_files --rawRead1 A1_1.fq --rawRead2 A1_2.fq --adapter /path/TruSeq3-PE.fa --somatic 5 --heritable 30 --outDir ./results --CORES 8"
 
 echo "================================================================================"
 echo 
@@ -84,6 +86,8 @@ done
 #  --adapter [file]	Path to Illumina adapter sequences file (e.g., TruSeq3-PE.fa)
 #  --outDir         Output directory where results will be generated
 #  --TEfam          Transposon family of Interest. The name should match the TE-specific filenames
+#  --somatic        Coverage threshold to call somatic insertions (Default = 5)
+#  --heritable      Coverage threshold to call non-reference segregating insertions (Default = 30)
 #  --CORES          Number of threads to use 
 #  --help | -h		Display this help message
 #####----------------------------------####### 
@@ -119,6 +123,8 @@ Sample=""
 adapter="/path/to/TruSeq3-PE.fa"   
 outDir="./deNOVOEnrich_output"    
 TEfam="AtCopiaX"
+somatic_cov=5
+heritable_cov=30
 monomeric_repeat=""            
 CORES=8 
 
@@ -135,6 +141,8 @@ while [[ "$#" -gt 0 ]]; do
         --adapter) adapter="$2"; shift ;;
         --outDir) outDir="$2"; shift ;;
         --TEfam) TEfam="$2"; shift ;;
+        --somatic) somatic_cov="$2"; shift;;
+        --heritable) heritable_cov="$2"; shift;;
         --monomeric_repeat) monomeric_repeat="$2"; shift ;;
         --help|-h) show_help; exit 0 ;;
         *) echo " Unknown parameter: $1"; show_help; exit 1 ;;
@@ -175,6 +183,8 @@ echo "Read 2 FASTQ           : $rawRead2"
 echo "Reference Genome       : $genome"
 echo "TE Reference Directory : $ref_TE"
 echo "TE Family              : $TEfam"
+echo "Somatic coverage cutoff : ≤$somatic_cov"
+echo "Heritable coverage cutoff : ≥$heritable_cov"
 echo "Adapter File           : $adapter"
 echo "Output Directory       : $outDir"
 echo "Threads (CORES)        : $CORES"
@@ -184,11 +194,10 @@ echo "==========================================================================
 
 
 ##Run pipeline##
-##Ia## Retrieving TE-specific reads and remove low Quality or read artefacts followed by Quality control and adapter trimming
+##Ia## Retrieving TE-specific reads and removing low-quality or read artefacts, followed by Quality control and adapter trimming
 echo ""
-echo "Retrieving TE-specific reads and removing low Quality or artefactual reads from raw sequencing data"
+echo "Retrieving TE-specific reads and removing low-quality or artefactual reads from raw sequencing data"
 echo ""
-#source activate denovoenrich
 
 cutadapt --discard-untrimmed -g file:$ref_TE/${TEfam}_flanking_sequence.fa -e 0.2 -o $outTrim/sub_1.fastq -p $outTrim/sub_2.fastq $rawRead1 $rawRead2 -O 25 -j $CORES --nextseq-trim=20
 
@@ -205,9 +214,8 @@ read2_count=$((read2_count / 4))
 
 
 ##Ib## Fetch fastq-read ID from trimmed fastq {Sample}s obtained after fastp run
-
-grep "@" $outTrim/sub_final_1a.fastq | sed 's/^@//; s/ /_/'g > $outTrim/sub_final_1a.fastqID.txt 
-grep "@" $outTrim/sub_final_2a.fastq | sed 's/^@//; s/ /_/'g > $outTrim/sub_final_2a.fastqID.txt 
+awk 'NR % 4 == 1 {sub(/^@/, "", $0); gsub(/ /, "_", $0); print}' $outTrim/sub_final_1a.fastq > $outTrim/sub_final_1a.fastqID.txt
+awk 'NR % 4 == 1 {sub(/^@/, "", $0); gsub(/ /, "_", $0); print}' $outTrim/sub_final_2a.fastq > $outTrim/sub_final_2a.fastqID.txt  
 
 id1_count=$(wc -l < "$outTrim/sub_final_1a.fastqID.txt") ##number should match the one in sub_final_1a.fastq
 id2_count=$(wc -l < "$outTrim/sub_final_2a.fastqID.txt") ##number should match the one in sub_final_2a.fastq
@@ -321,24 +329,22 @@ picard MarkDuplicates REMOVE_DUPLICATES=false I=$outDir/${Sample}_raw_sub_final_
 samtools index $outDir/${Sample}_raw_sub_final_norm.assembled_${TEfam}_ext.sorted.dedup.bam $outDir/${Sample}_raw_sub_final_norm.assembled_${TEfam}_ext.sorted.dedup.bai
 
 echo ""
-echo "You now have Reference and TE ALignments"
+echo "You now have Reference and TE Alignments"
 echo ""
 echo "============================================================================="
 
-#conda deactivate
 ##Va## Tagging Alignments with Readtagger (target: Reference; source: TE_extremity)
 
 echo ""
 echo "Tagging Genome:TE alignments"
 
-#source activate readtagger
+
 readtagger -t $outDir/${Sample}_raw_sub_final_${TEfam}_norm.assembled.sorted.uniq.split.dedup.bam -s $outDir/${Sample}_raw_sub_final_norm.assembled_${TEfam}_ext.sorted.dedup.bam -o $outDir/${Sample}_${TEfam}.uniq.split.readtagged.bam --cores $CORES 
 
-#conda deactivate
 
 ##Vb## Retrieving read-tagged alignment records for peak calling
 
-#source activate denovoenrich
+
 samtools view -H --threads $CORES $outDir/${Sample}_${TEfam}.uniq.split.readtagged.bam > $outDir/${Sample}_${TEfam}.uniq.split.readtagged.final.bam
 
 samtools view $outDir/${Sample}_${TEfam}.uniq.split.readtagged.bam | grep "${TEfam}" >> $outDir/${Sample}_${TEfam}.uniq.split.readtagged.final.bam
@@ -352,11 +358,11 @@ echo ""
 
 
 ##VIa## Peak calling
-##Using the bamtobed Samples with PCR duplicates: this will allow us to retrive all the unique reads which will be putative insertions and if a particular insertion events has clonal duplications or not. One with very high pcr duplicates will be fixed insertion. Other with low or no duplicates will be somatic insertions
+##Using the bamtobed Samples with PCR duplicates: this will allow us to retrieve all the unique read,s which will be putative insertions, and if a particular insertion event has clonal duplications or not. 
 
 bedtools bamtobed -i $outDir/${Sample}_${TEfam}.uniq.split.readtagged.final.bam -split > $outDir/${Sample}_${TEfam}.uniq.split.readtagged.final.bamtobed.bed
 
-sort -k1,1 -k2,2n $outDir/${Sample}_${TEfam}.uniq.split.readtagged.final.bamtobed.bed  | awk ' $1 !="ChrM" && $1 != "ChrC" {print $0}' > $outDir/${Sample}_${TEfam}.uniq.split.readtagged.final.bamtobed.sorted.bed ## 6 column bed: chr start stop readID score strand
+sort -k1,1 -k2,2n $outDir/${Sample}_${TEfam}.uniq.split.readtagged.final.bamtobed.bed  | awk ' $1 !="ChrM" && $1 != "ChrC" {print $0}' > $outDir/${Sample}_${TEfam}.uniq.split.readtagged.final.bamtobed.sorted.bed 
 
 awk '{print $1"\t"$2"\t"$3"\t"$6}' $outDir/${Sample}_${TEfam}.uniq.split.readtagged.final.bamtobed.sorted.bed | awk '{count[$0]++} END {for (line in count) print line, count[line]}'| sort -k1,1 -k2,2n | sed 's/ /\t/g' > $outDir/${Sample}_${TEfam}.uniq.split.readtagged.final.bamtobed.sorted.withPCRdupCount.bed 
 
@@ -364,20 +370,18 @@ awk '$4=="+" {print $1"\t"$2"\t"$4"\t"$5}' $outDir/${Sample}_${TEfam}.uniq.split
 
 awk '$4=="-" {print $1"\t"$3"\t"$4"\t"$5}' $outDir/${Sample}_${TEfam}.uniq.split.readtagged.final.bamtobed.sorted.withPCRdupCount.bed | sort -k1,1 -k2,2n | bedtools groupby -i stdin -g 1,2,3 -c 4 -o sum,count,collapse >> $outDir/${Sample}_${TEfam}.uniq.split.readtagged.final.bamtobed.startsite.bed
 
-sort -k1,1 -k2,2n $outDir/${Sample}_${TEfam}.uniq.split.readtagged.final.bamtobed.startsite.bed > $outDir/${Sample}_${TEfam}.uniq.split.readtagged.final.bamtobed.startsite.sorted.bed ##this will generate a 6 bed Sample for start positions: chr start_position strand sumofallreads_includePCRduplicates countofreadswithcommonstartbutdiffstopsite comma_sperated_list_of_ind_pcrdup_counts
+sort -k1,1 -k2,2n $outDir/${Sample}_${TEfam}.uniq.split.readtagged.final.bamtobed.startsite.bed > $outDir/${Sample}_${TEfam}.uniq.split.readtagged.final.bamtobed.startsite.sorted.bed
 
 
 
 
-##VIb## Calling fixed insertions: conditional filtering for non-reference fixed loci: threshold coverage >=30, minimum number of unique reads at the insertion site >=5 and from the unique reads atleast 3 of them will have pcr duplicates of 3 or more #
-
+##VIb## Calling fixed insertions: conditional filtering for non-reference segregating heritable loci: threshold coverage default >=30, minimum number of unique reads at the insertion site >=5 and from the unique reads atleast 3 of them will have pcr duplicates of 3 or more #
 
  
-awk -F'\t' '{split($6, values, ","); count = 0; for (i in values) {if (values[i] >= 3) {count++;}} if ($4 >= 30 && $5 >= 5 && count >= 3) {print $0;}}' $outDir/${Sample}_${TEfam}.uniq.split.readtagged.final.bamtobed.startsite.sorted.bed > $outDir/${Sample}_${TEfam}.putative.fixed_insertions.bed
+awk -F'\t' "{split(\$6, values, \",\"); count = 0; for (i in values) {if (values[i] >= 3) {count++;}} if (\$4 >= ${heritable_cov} && \$5 >= 5 && count >= 3) {print \$0;}}" $outDir/${Sample}_${TEfam}.uniq.split.readtagged.final.bamtobed.startsite.sorted.bed > $outDir/${Sample}_${TEfam}.putative.fixed_insertions.bed
 
-##convert into bed first here you have only start coordinates also you need to take care of the strand info in the last call whether the TE seq is direct or reverse complement
 
-awk '{{if ($3=="-") print $1"\t"$2-1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6} if( $3=="+")  print $1"\t"$2"\t"$2+1"\t"$3"\t"$4"\t"$5"\t"$6}' $outDir/${Sample}_${TEfam}.putative.fixed_insertions.bed > $outDir/${Sample}_${TEfam}.putative.fixed_insertions.temp1.bed #7 column bed: chr start stop  strand total_coverage Total_unique reads duplicates_distribution_per_unique_read 
+awk '{{if ($3=="-") print $1"\t"$2-1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6} if( $3=="+")  print $1"\t"$2"\t"$2+1"\t"$3"\t"$4"\t"$5"\t"$6}' $outDir/${Sample}_${TEfam}.putative.fixed_insertions.bed > $outDir/${Sample}_${TEfam}.putative.fixed_insertions.temp1.bed  
 
 
 ##VIc## Filtering putative insertions: removing insertions within 1kb of native loci and those within 100bp of mono-nucleotide trails
@@ -385,9 +389,9 @@ awk '{{if ($3=="-") print $1"\t"$2-1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6} if( $3=="+")
 
 bedtools window -v -w 1000 -a $outDir/${Sample}_${TEfam}.putative.fixed_insertions.temp1.bed  -b $ref_TE/${TEfam}_loci.bed > $outDir/${Sample}_${TEfam}.fixed_insertions.temp2.bed
 
-bedtools window -v -w 50 -a $outDir/${Sample}_${TEfam}.fixed_insertions.temp2.bed -b $monomeric_repeat > $outDir/${Sample}_${TEfam}.fixed_insertions.final.bed ##use this file in case bed format is required for intersection with other features
+bedtools window -v -w 50 -a $outDir/${Sample}_${TEfam}.fixed_insertions.temp2.bed -b $monomeric_repeat > $outDir/${Sample}_${TEfam}.fixed_insertions.final.bed 
 
-awk '{{if ($4=="-") print $1"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7} if( $4=="+")  print $1"\t"$2"\t"$4"\t"$5"\t"$6"\t"$7}' $outDir/${Sample}_${TEfam}.fixed_insertions.final.bed > $outDir/${Sample}_${TEfam}.fixed_insertions.final.startsite.bed ##use this Sample if a specific insertion site is required
+awk '{{if ($4=="-") print $1"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7} if( $4=="+")  print $1"\t"$2"\t"$4"\t"$5"\t"$6"\t"$7}' $outDir/${Sample}_${TEfam}.fixed_insertions.final.bed > $outDir/${Sample}_${TEfam}.fixed_insertions.final.startsite.bed 
 
 heritable_count=$(wc -l < "$outDir/${Sample}_${TEfam}.fixed_insertions.final.bed")
 
@@ -403,30 +407,30 @@ echo ""
 awk '$5==1 && $4==1 {print $0}' $outDir/${Sample}_${TEfam}.uniq.split.readtagged.final.bamtobed.startsite.sorted.bed > $outDir/${Sample}_${TEfam}.putative_rare_soma_insertions.bed
 awk '{{if ($3=="-") print $1"\t"$2-1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6} if( $3=="+")  print $1"\t"$2"\t"$2+1"\t"$3"\t"$4"\t"$5"\t"$6}' $outDir/${Sample}_${TEfam}.putative_rare_soma_insertions.bed > $outDir/${Sample}_${TEfam}.putative_rare_soma_insertions.temp1.bed
 bedtools window -v -w 1000 -a $outDir/${Sample}_${TEfam}.putative_rare_soma_insertions.temp1.bed  -b $ref_TE/${TEfam}_loci.bed > $outDir/${Sample}_${TEfam}.putative_rare_soma_insertions.temp2.bed
-bedtools window -v -w 50 -a $outDir/${Sample}_${TEfam}.putative_rare_soma_insertions.temp2.bed -b $monomeric_repeat > $outDir/${Sample}_${TEfam}_rare_soma_insertions.final.bed ##use this Sample in case bed format is required for intersection with other features
-awk '{{if ($4=="-") print $1"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7} if( $4=="+")  print $1"\t"$2"\t"$4"\t"$5"\t"$6"\t"$7}' $outDir/${Sample}_${TEfam}_rare_soma_insertions.final.bed > $outDir/${Sample}_${TEfam}_rare_soma_insertions.final.startsite.bed ##use this Sample if a specific insertion site is required
+bedtools window -v -w 50 -a $outDir/${Sample}_${TEfam}.putative_rare_soma_insertions.temp2.bed -b $monomeric_repeat > $outDir/${Sample}_${TEfam}_rare_soma_insertions.final.bed 
+awk '{{if ($4=="-") print $1"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7} if( $4=="+")  print $1"\t"$2"\t"$4"\t"$5"\t"$6"\t"$7}' $outDir/${Sample}_${TEfam}_rare_soma_insertions.final.bed > $outDir/${Sample}_${TEfam}_rare_soma_insertions.final.startsite.bed 
 
 
-##recent somatic insertions where at least 1 or more PCR duplicate is available for the insertion site
+##recent somatic insertions where at least 1 or more PCR duplicates are available for the insertion site
 awk '$5==1 && $4>1 {print $0}' $outDir/${Sample}_${TEfam}.uniq.split.readtagged.final.bamtobed.startsite.sorted.bed > $outDir/${Sample}_${TEfam}.putative_recent_soma_insertions.bed
 awk '{{if ($3=="-") print $1"\t"$2-1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6} if( $3=="+")  print $1"\t"$2"\t"$2+1"\t"$3"\t"$4"\t"$5"\t"$6}' $outDir/${Sample}_${TEfam}.putative_recent_soma_insertions.bed > $outDir/${Sample}_${TEfam}.putative_recent_soma_insertions.temp1.bed
 bedtools window -v -w 1000 -a $outDir/${Sample}_${TEfam}.putative_recent_soma_insertions.temp1.bed  -b $ref_TE/${TEfam}_loci.bed > $outDir/${Sample}_${TEfam}.putative_recent_soma_insertions.temp2.bed
-bedtools window -v -w 50 -a $outDir/${Sample}_${TEfam}.putative_recent_soma_insertions.temp2.bed -b $monomeric_repeat > $outDir/${Sample}_${TEfam}_recent_soma_insertions.final.bed ##use this Sample in case bed format is required for intersection with other features
-awk '{{if ($4=="-") print $1"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7} if( $4=="+")  print $1"\t"$2"\t"$4"\t"$5"\t"$6"\t"$7}' $outDir/${Sample}_${TEfam}_recent_soma_insertions.final.bed > $outDir/${Sample}_${TEfam}_recent_soma_insertions.final.startsite.bed ##use this Sample if a specific insertion site is required
+bedtools window -v -w 50 -a $outDir/${Sample}_${TEfam}.putative_recent_soma_insertions.temp2.bed -b $monomeric_repeat > $outDir/${Sample}_${TEfam}_recent_soma_insertions.final.bed 
+awk '{{if ($4=="-") print $1"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7} if( $4=="+")  print $1"\t"$2"\t"$4"\t"$5"\t"$6"\t"$7}' $outDir/${Sample}_${TEfam}_recent_soma_insertions.final.bed > $outDir/${Sample}_${TEfam}_recent_soma_insertions.final.startsite.bed 
 
 
 ##primary somatic insertions (occurred very early in development/experimental phase) where more than 1 unique reads with 1 or more pcr duplicates are available 
 awk '$5 < 5 && $5 > 1 && $4 >= 2 * $5 && $4 < 30 {print $0}' $outDir/${Sample}_${TEfam}.uniq.split.readtagged.final.bamtobed.startsite.sorted.bed > $outDir/${Sample}_${TEfam}.putative_early_soma_insertions.bed
 awk '{{if ($3=="-") print $1"\t"$2-1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6} if( $3=="+")  print $1"\t"$2"\t"$2+1"\t"$3"\t"$4"\t"$5"\t"$6}' $outDir/${Sample}_${TEfam}.putative_early_soma_insertions.bed > $outDir/${Sample}_${TEfam}.putative_early_soma_insertions.temp1.bed
 bedtools window -v -w 1000 -a $outDir/${Sample}_${TEfam}.putative_early_soma_insertions.temp1.bed  -b $ref_TE/${TEfam}_loci.bed > $outDir/${Sample}_${TEfam}.putative_early_soma_insertions.temp2.bed
-bedtools window -v -w 50 -a $outDir/${Sample}_${TEfam}.putative_early_soma_insertions.temp2.bed -b $monomeric_repeat > $outDir/${Sample}_${TEfam}_early_soma_insertions.final.bed ##use this Sample in case bed format is required for intersection with other features
-awk '{{if ($4=="-") print $1"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7} if( $4=="+")  print $1"\t"$2"\t"$4"\t"$5"\t"$6"\t"$7}' $outDir/${Sample}_${TEfam}_early_soma_insertions.final.bed > $outDir/${Sample}_${TEfam}_early_soma_insertions.final.startsite.bed ##use this Sample if a specific insertion site is required
+bedtools window -v -w 50 -a $outDir/${Sample}_${TEfam}.putative_early_soma_insertions.temp2.bed -b $monomeric_repeat > $outDir/${Sample}_${TEfam}_early_soma_insertions.final.bed 
+awk '{{if ($4=="-") print $1"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7} if( $4=="+")  print $1"\t"$2"\t"$4"\t"$5"\t"$6"\t"$7}' $outDir/${Sample}_${TEfam}_early_soma_insertions.final.bed > $outDir/${Sample}_${TEfam}_early_soma_insertions.final.startsite.bed 
 
 ##VIIb##combine all soma insertions: Total somatic insertions
 
 cat $outDir/${Sample}_${TEfam}_*_soma_insertions.final.bed | sort -k1,1 -k2,2n > $outDir/${Sample}_${TEfam}_All_soma_insertions.final.bed
 
-awk '$5<=5 {print $0}' $outDir/${Sample}_${TEfam}_All_soma_insertions.final.bed > $outDir/${Sample}_${TEfam}.somatic_insertions.final.bed
+awk "\$5 <= ${somatic_cov} {print \$0}" $outDir/${Sample}_${TEfam}_All_soma_insertions.final.bed > $outDir/${Sample}_${TEfam}.somatic_insertions.final.bed
 
 somatic_count=$(wc -l < "$outDir/${Sample}_${TEfam}.somatic_insertions.final.bed")
 
@@ -440,8 +444,7 @@ samtools view -hb $outDir/${Sample}_${TEfam}.uniq.split.readtagged.final.bam > $
 
 samtools index $outDir/${Sample}_${TEfam}.uniq.split.tagged_alignments.final.bam
 
-
-#conda deactivate 
+ 
 rm $outDir/*.temp*.bed
 rm -r $outTrim
 
